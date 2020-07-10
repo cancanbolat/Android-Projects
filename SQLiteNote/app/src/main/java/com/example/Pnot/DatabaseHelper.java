02dc7bee39101a7fdf52cfcddd361aa3e71592c9
= <=>
2020-07-10 14:15:36 +0300

    Files transferred to git

---
+++ SQLiteNote/app/src/main/java/com/example/Pnot/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.Pnot;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    //Table Name
+    public static final String TABLE_NAME = "COUNTRIES";
+
+    //Table Columns
+    public static final String _ID = "_id";
+    public static final String SUBJECT = "subject";
+    public static final String DESC = "description";
+
+    //Database Information
+    static  final String DB_NAME = "MASTER_ANDROID_APP.DB";
+
+    //Database Version
+    static final int DB_VERSION = 1;
+
+    //Crating table query
+    private static final String CREATE_TABLE = "create table " + TABLE_NAME +
+            "(" + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            SUBJECT + " TEXT NOT NULL, " + DESC + " TEXT);";
+
+    //Constructor
+    public DatabaseHelper (Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Executing the Query
+        db.execSQL(CREATE_TABLE);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME );
+        onCreate(db);
+    }
+}
