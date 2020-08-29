01fe6fcc45fb9bc19d1cc3dbc7c303644edc2ba3
Can Canbolat <65857296+cancanbolat@users.noreply.github.com>
2020-08-29 12:06:40 +0300

    Update list.php
    array_push was used for json_encode

--- Post and Details App - Retrofit/Php & Sql Files/list.php
+++ Post and Details App - Retrofit/Php & Sql Files/list.php
@@ -2,44 +2,24 @@
 
 require_once "connection.php";
 
-
-Class posts{
-    public $blog_id;
-    public $blog_name;
-    public $blog_detail;
-    public $blog_small_detail;
-}
-
-$po = new posts();
-
 $query = $db->prepare("SELECT * FROM blog");
-$query->execute(array());
-$counts = $db->query("SELECT COUNT(*) FROM blog")->fetchColumn();
+$query->execute();
 
-$cou = 0;
-
-echo "[";
+$response = array();
 
 while($row = $query->fetch(PDO::FETCH_ASSOC)){
     
-    $cou = $cou+1;
-    $po->blog_id     = $row["blog_id"];
-    $po->blog_name   = $row["blog_name"];
-    $po->blog_detail = $row["blog_detail"];
-    $po->blog_small_detail = $row["blog_small_detail"];
-    
-    
-    echo json_encode($po,JSON_UNESCAPED_UNICODE);
-    
-    if($cou != ($counts) ){
-        echo ",";
-    }
-    
-    
-    
+    array_push($response, 
+               array(
+              'blog_id'     => $row["blog_id"],
+              'blog_name'   => $row["blog_name"],
+              'blog_detail' => $row["blog_detail"],
+              'blog_small_detail' => $row["blog_small_detail"]
+               )
+   );   
 }
 
-echo "]";
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
 
 ?>
 
